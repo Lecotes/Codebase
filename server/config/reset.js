23b5560b4c23ff65404d8872130d895146e7871d
Dorisa Shehi <doris.shehi2@gmail.com>
2024-11-12 21:15:07 -0500

    reset for materials table added

---
+++ server/config/reset.js
@@ -0,0 +1,55 @@
+import { pool } from "./database.js";
+
+import "./dotenv.js";
+
+const createMaterialsTable = async () => {
+  const createTableQuery = `
+            DROP TABLE IF EXISTS materials CASCADE;
+
+            CREATE TABLE IF NOT EXISTS materials (
+              id SERIAL PRIMARY KEY,
+              group_id INT NOT NULL,
+              user_id INT NOT NULL
+              title VARCHAR(255) NOT NULL,
+              content TEXT NOT NULL,
+              created_at TIMESTAMP DEFAUT Now(),
+              updated_at TIMESTAMP DEFAUT Now(),
+            )
+        `;
+
+  try {
+    const res = await pool.query(createTableQuery);
+    console.log("🎉 materials table created successfully");
+  } catch (err) {
+    console.error("⚠️ error creating materials table", err);
+  }
+};
+
+const seedTableMaterials = async () => {
+  await createMaterialsTable();
+
+  materials.forEach((material) => {
+    const insertQuery = {
+      text: "INSERT INTO materials (group_id, user_id, title, contentti) VALUES ($1, $2, $3, $4)",
+    };
+
+    const values = [
+      material.group_id,
+      material.user_id,
+      material.title,
+      material.content,
+    ];
+
+    pool.query(insertQuery, values, (err, res) => {
+      if (err) {
+        console.error("⚠️ error inserting materials table", err);
+        return;
+      }
+
+      console.log(`✅ ${material.title} added successfully`);
+    });
+  });
+};
+wheelwheelwheelwheel;
+
+seedTableMaterials();
