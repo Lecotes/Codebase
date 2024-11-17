4379c27209c9468da797b425d6135697c46b1780
Dorisa Shehi <doris.shehi2@gmail.com>
2024-11-16 22:04:43 -0500

    Text stored into database

--- frontend/src/pages/UploadsPage.jsx
+++ frontend/src/pages/UploadsPage.jsx
@@ -49,8 +49,11 @@ const Uploads = () => {
       },
       body: JSON.stringify(documentData),
     };
-    const response = fetch("/api/material/create", options);
-    window.location = "/";
+    const response = fetch(
+      "http://localhost:3000/api/material/create",
+      options
+    );
+    //window.location = "/";
   };
 
   return (
